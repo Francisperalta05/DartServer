43ba3ab63c1daa959f1efe7712885d9d43170c08
Angel Peralta <106847324+AngelFPeralta@users.noreply.github.com>
2024-05-03 23:20:17 -0400

    Rename url

--- bin/server.dart
+++ bin/server.dart
@@ -7,14 +7,14 @@ import 'package:dart_server/api/api_server.dart';
 import 'package:dart_server/mongo_connection.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
-const _hostname = 'localhost';
+const _hostname = '148.0.10.36';
 
 void main(List<String> args) async {
   final parser = ArgParser()..addOption('port', abbr: 'p');
   final result = parser.parse(args);
 
   // For Google Cloud Run, we respect the PORT environment variable
-  var portStr = result['port'] ?? Platform.environment['PORT'] ?? '8080';
+  var portStr = result['port'] ?? Platform.environment['PORT'] ?? '80';
   var port = int.tryParse(portStr);
 
   if (port == null) {
